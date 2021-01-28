343ba0a25b4be3d25a8cb092615de90174fb216d
Blagoslav Mihaylov <az@blago.io>
2021-01-28 22:51:14 +0200

    Added example protocol data on verify section

--- src/components/process_protocols/ProcessProtocols.js
+++ src/components/process_protocols/ProcessProtocols.js
@@ -6,41 +6,97 @@ import { ContentPanel } from '../sections/Sections';
 import ProtocolPhotos from './ProtocolPhotos';
 import VerifyProtocolInfo from './VerifyProtocolInfo';
 
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+import Loading from '../layout/Loading';
+
+const ReadyScreen = styled.div`
+    max-width: 900px;
+    display: block;
+    margin: 0 auto;
+    padding: 50px;
+    //border: 1px solid #aaa;
+    border-radius: 50px;
+    //margin-top: 30px;
+
+    hr {
+        margin: 20px 0;
+        border: 1px solid #ddd;
+        border-top: 0;
+    }
+`;
+
+const NextProtocolButton = styled.button`
+    border: none;
+    background-color: #22c122;
+    color: white;
+    padding: 20px 50px;
+    font-size: 36px;
+    cursor: pointer;
+    margin: 0 auto;
+    border-radius: 20px;
+    border-bottom: 10px solid #118a00;
+    display: block;
+    margin-top: 60px;
+    box-sizing: border-box;
+    position: relative;
+    top: 0;
+
+    &:hover {
+        background-color: #2ece2e;
+    }
+
+    &:active {
+        top: 10px;
+        border-bottom: 0;
+        margin-bottom: 10px;
+    }
+`;
+
 export default props => {
     const [protocol, setProtocol] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     const nextProtocol = () => {
-        setProtocol({
-            photos: [
-                '234602054-01.png',
-                '234602054-02.png',
-                '234602054-03.png',
-                '234602054-04.png',
-                '234602054-05.png',
-                '234602054-06.png',
-                '234602054-07.png',
-                '234602054-08.png',
-                '234602054-09.png',
-                '234602054-10.png',
-                '234602054-11.png',
-                '234602054-12.png',
-                '234602054-13.png',
-                '234602054-14.png',
-            ]
-        });
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+            setProtocol({
+                photos: [
+                    '234602054-01.png',
+                    '234602054-02.png',
+                    '234602054-03.png',
+                    '234602054-04.png',
+                    '234602054-05.png',
+                    '234602054-06.png',
+                    '234602054-07.png',
+                    '234602054-08.png',
+                    '234602054-09.png',
+                    '234602054-10.png',
+                    '234602054-11.png',
+                    '234602054-12.png',
+                    '234602054-13.png',
+                    '234602054-14.png',
+                ]
+            });
+        }, 2000);
     };
 
     return(
+        loading? <Loading/> :
         !protocol?
-            <div>
-                <h1>Обработка на протоколи</h1>
-                <Link to="/protocols">Обратно</Link>
+            <ReadyScreen>
+                <h1 style={{textAlign: 'center', fontSize: '54px'}}>Обработка на протоколи</h1>
                 <hr/>
                 <p>
                     Когато сте готови, натиснете долу и ще ви бъде назначен протокол.
                 </p>
-                <button onClick={nextProtocol}>Следващ протокол</button>
-            </div> :
+                <NextProtocolButton onClick={nextProtocol}>
+                    <FontAwesomeIcon icon={faFile}/> Следващ протокол
+                </NextProtocolButton>
+                <Link to="/protocols" style={{textAlign: 'center', display: 'block'}}>Върнете се обратно</Link>
+            </ReadyScreen> :
             <VerifyProtocolInfo protocol={protocol} returnProtocol={() => setProtocol(null)}/>
     );
 };
--- src/components/process_protocols/VerifyProtocolInfo.js
+++ src/components/process_protocols/VerifyProtocolInfo.js
@@ -14,6 +14,16 @@ const ProtocolInfoSection = styled.div`
     right: 0;
 `;
 
+const ProtocolDetails = styled.div`
+    padding: 20px;
+
+    hr {
+        margin: 20px 0;
+        border: 1px solid #ddd;
+        border-top: 0;
+    }
+`;
+
 const SectionHeader = styled.div`
     //padding: 10px;
     background-color: rgb(56,222,203);
@@ -33,6 +43,36 @@ const BackButton = styled.button`
 `;
 
 export default props => {
+
+    const partyDropDown = selectedIndex => {
+        const parties = [
+            'ПАТРИОТИ ЗА ВАЛЕРИ СИМЕОНОВ',
+            'ПЪТ НА МЛАДИТЕ (НДСВ и НОВОТО ВРЕМЕ)',
+            'ПП ВМРО – БЪЛГАРСКО НАЦИОНАЛНО ДВИЖЕНИЕ',
+            'ИК за Ваня Руменова Григорова',
+            'партия на ЗЕЛЕНИТЕ',
+            'ПП ВОЛТ',
+            'ВЪЗХОД	1',
+            'ИК за Десислава Петрова Иванчева',
+            'ПП ГЕРБ',
+            'ДЕМОКРАТИЧНА БЪЛГАРИЯ – ОБЕДИНЕНИЕ',
+            'АТАКА',
+            'ИК за Минчо Христов Куминев',
+            'БСП ЗА БЪЛГАРИЯ',
+            'Движение за права и свободи - ДПС',
+            'ВОЛЯ – Българските Родолюбци',
+            'Движение 21',
+            'ПП „ДВИЖЕНИЕ ПРЕЗАРЕДИ БЪЛГАРИЯ“',
+            'ПП „Възраждане“',
+        ];
+
+        return(
+            <select>
+                {parties.map((party, i) => <option selected={i===selectedIndex}>{party}</option>)}
+            </select>
+        );
+    };
+
     return(
         <div>
             <ProtocolPhotos protocol={props.protocol}/>
@@ -43,15 +83,275 @@ export default props => {
                     </BackButton>
                     <h1 style={{display: 'inline-block'}}>Секция 234602054</h1>
                 </SectionHeader>
-                <h1>Общи данни</h1>
-                <button>Приеми</button>
-                <button>Отхвърли</button>
-                <table>
-
-                </table>
-                <hr/>
-                <button>Приеми протокол</button>
-                <button>Отхвърли протокол</button>
+                <ProtocolDetails>
+                    <h1>СЕКЦИОННАТА ИЗБИРАТЕЛНА КОМИСИЯ УСТАНОВИ</h1>
+                    <table>
+                        <tr>
+                            <td>А. Брой на бюлетините, получени по реда на чл. 215, ал. 1, т. 2 от ИК</td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value="700"
+                                />
+                            </td>
+                        </tr>
+                    </table>
+                    <button>Приеми</button>
+                    <button>Отхвърли</button>
+                    <hr/>
+                    <h1>ДАННИ ОТ ИЗБИРАТЕЛНИ СПИСЪЦИ</h1>
+                    <table>
+                        <tr>
+                            <td>1. Брой на избирателите според част І и част ІІ на избирателния списък при предаването му на СИК</td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value="677"
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2. Брой на избирателите, вписани в допълнителната страница (под чертата) на избирателния списък (част І и част ІІ) в изборния ден</td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value="6"
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>3. Брой на гласувалите избиратели според положените подписи в избирателния списък (част І и част ІІ), включително и подписите в допълнителната страница/и (под чертата)</td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value="245"
+                                />
+                            </td>
+                        </tr>
+                    </table>
+                    <button>Приеми</button>
+                    <button>Отхвърли</button>
+                    <hr/>
+                    <h1>ДАННИ ИЗВЪН ИЗБИРАТЕЛНИ СПИСЪЦИ И СЪДЪРЖАНИЕТО НА ИЗБИРАТЕЛНАТА КУТИЯ</h1>
+                    <h2>4. Бюлетини извън избирателната кутия</h2>
+                    <table>
+                        <tr>
+                            <td>4.а) брой на неизползваните бюлетини</td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value="454"
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>4.б) брой на унищожените от СИК бюлетини по други поводи (за създаване на образци за таблата пред изборното помещение и увредени механично при откъсване от кочана и станали неизползваеми) </td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value="1"
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>4.в) брой на недействителните бюлетини по чл. 265, ал. 5 от ИК (когато номерът на бюлетината не съответства на номер в кочана)</td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value="0"
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>4.г) брой на недействителните бюлетини по чл. 227 от ИК (при които е използвана възпроизвеждаща техника) </td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value="0"
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>4.д) брой на недействителните бюлетини по чл. 228 от ИК (показан публично вот след гласуване)</td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value="0"
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>4.е) брой на сгрешените бюлетини</td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value="0"
+                                />
+                            </td>
+                        </tr>
+                    </table>
+                    <button>Приеми</button>
+                    <button>Отхвърли</button>
+                    <hr/>
+                    <h1>СЛЕД КАТО ОТВОРИ ИЗБИРАТЕЛНАТА КУТИЯ, СИК УСТАНОВИ</h1>
+                    <table>
+                        <tr>
+                            <td>5. Брой на намерените в избирателната кутия бюлетини</td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value="245"
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>6. Брой намерени в избирателната кутия недействителни гласове (бюлетини)</td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value="0"
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>7. Общ брой намерени в избирателната кутия действителни гласове (бюлетини)</td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value="245"
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>7.1 брой на действителните гласове, подадени за кандидатските листи на партии, коалиции и инициативни комитети </td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value="245"
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>7.2 брой действителни гласове (отбелязвания) само в квадратчето „Не подкрепям никого“</td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value="0"
+                                />
+                            </td>
+                        </tr>
+                    </table>
+                    <button>Приеми</button>
+                    <button>Отхвърли</button>
+                    <hr/>
+                    <h1>8. РАЗПРЕДЕЛЕНИЕ НА ГЛАСОВЕТЕ ПО КАНДИДАТСКИ ЛИСТИ</h1>
+                    <table>
+                        <tr>
+                            <td>{partyDropDown(0)}</td>
+                            <td><input type="text" value="3"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(1)}</td>
+                            <td><input type="text" value="3"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(2)}</td>
+                            <td><input type="text" value="12"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(3)}</td>
+                            <td><input type="text" value="1"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(4)}</td>
+                            <td><input type="text" value="2"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(5)}</td>
+                            <td><input type="text" value="1"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(6)}</td>
+                            <td><input type="text" value="1"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(7)}</td>
+                            <td><input type="text" value="13"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(8)}</td>
+                            <td><input type="text" value="52"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(9)}</td>
+                            <td><input type="text" value="48"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(10)}</td>
+                            <td><input type="text" value="4"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(11)}</td>
+                            <td><input type="text" value="10"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(12)}</td>
+                            <td><input type="text" value="60"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(13)}</td>
+                            <td><input type="text" value="1"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(14)}</td>
+                            <td><input type="text" value="4"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(15)}</td>
+                            <td><input type="text" value="21"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(16)}</td>
+                            <td><input type="text" value="1"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td>{partyDropDown(17)}</td>
+                            <td><input type="text" value="8"/></td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                        <tr>
+                            <td colSpan="3">
+                                <button>Добави</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan="2">9. Бюлетини, в които не е отбелязан вотът на избирателя (празни бюлетини), бюлетини, в които е гласувано в повече от едно квадратче; бюлетини, в които не може да се установи еднозначно вотът на избирателя и други видове недействителни гласове</td>
+                            <td><input type="text" value="0"/></td>
+                        </tr>
+                    </table>
+                    <button>Приеми</button>
+                    <button>Отхвърли</button>
+                    <hr/>
+                    <button>Приеми протокол</button>
+                    <button>Отхвърли протокол</button>
+                </ProtocolDetails>
             </ProtocolInfoSection>
         </div>
     );
