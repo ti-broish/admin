e2b86fb43539aa5554c41e457b3402d648338940
Blagoslav Mihaylov <az@blago.io>
2021-01-31 14:54:57 +0200

    Styled protocol verification section

--- src/components/process_protocols/VerifyProtocolInfo.js
+++ src/components/process_protocols/VerifyProtocolInfo.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import ProtocolPhotos from './ProtocolPhotos';
 
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faPlus, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const ProtocolInfoSection = styled.div`
     width: 50vw;
@@ -16,6 +17,15 @@ const ProtocolInfoSection = styled.div`
 
 const ProtocolDetails = styled.div`
     padding: 20px;
+
+    h1 {
+        margin: 10px 0;
+        font-size: 22px;
+    }
+
+    h2 {
+        font-size: 18px;
+    }
 
     hr {
         margin: 20px 0;
@@ -40,6 +50,138 @@ const BackButton = styled.button`
     padding: 15px;
     border-right: 1px solid white;
     margin-right: 20px;
+`;
+
+const AcceptButton = styled.button`
+    border: none;
+    background-color: #44e644;
+    color: white;
+    padding: 5px 10px;
+    font-size: 16px;
+    cursor: pointer;
+    border-radius: 5px;
+    border-bottom: 3px solid #2eae1c;
+    display: block;
+    box-sizing: border-box;
+    display: inline-block;
+    font-weight: bold;
+    width: 115px;
+    margin-top: 30px;
+
+    &:hover {
+        background-color: #2ece2e;
+    }
+
+    &:active {
+        top: 10px;
+        border-bottom: 0;
+        margin-bottom: 10px;
+    }
+`;
+
+const RejectButton = styled.button`
+    border: none;
+    background-color: #ff4545;
+    color: white;
+    padding: 5px 10px;
+    font-size: 16px;
+    cursor: pointer;
+    border-radius: 5px;
+    border-bottom: 3px solid #ce4c4c;
+    display: block;
+    box-sizing: border-box;
+    display: inline-block;
+    font-weight: bold;
+    width: 115px;
+    margin-top: 20px;
+    margin-left: 5px;
+
+    &:hover {
+        background-color: #2ece2e;
+    }
+
+    &:active {
+        top: 10px;
+        border-bottom: 0;
+        margin-bottom: 10px;
+    }
+`;
+
+const ProtocolDetailsTable = styled.table`
+    table-layout: fixed;
+
+    button { 
+        width: 100%; 
+        box-sizing: border-box;
+    }
+
+    input { 
+        width: 100%; 
+        box-sizing: border-box;
+        width: 100%;
+        box-sizing: border-box;
+        border: 1px solid #ddd;
+        padding: 8px;
+        border-top: 2px solid #ddd;
+        border-radius: 10px;
+        text-align: right;
+    }
+    
+    select { 
+        width: 100%; 
+        box-sizing: border-box;
+    }
+
+    td:nth-child(1) { width: 80%; }
+    td:nth-child(2) { width: 20%; }
+`;
+
+const svgIcon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#ccc" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg>';
+
+const PartyResultsTable = styled.table`
+    table-layout: fixed;
+    width: 100%;
+
+    button { 
+        width: 100%; 
+        box-sizing: border-box;
+        background-color: #aaa;
+        padding: 6px;
+        border-radius: 10px;
+        border: none;
+        color: white;
+        font-weight: bold;
+        cursor: pointer;
+    }
+
+    input { 
+        width: 100%; 
+        box-sizing: border-box;
+        width: 100%;
+        box-sizing: border-box;
+        border: 1px solid #ddd;
+        padding: 2px 8px;
+        border-top: 2px solid #ddd;
+        border-radius: 5px;
+        text-align: right;
+    }
+    
+    select { 
+        width: 100%; 
+        box-sizing: border-box;
+        appearance: none;
+        border: 1px solid #ccc;
+        border-bottom: 2px solid #ccc;
+        border-radius: 5px;
+        padding: 3px;
+        background: url("data:image/svg+xml;base64,${window.btoa(svgIcon)}") no-repeat;
+        background-position: right 5px top 50%;
+        cursor: pointer;
+    }
+
+    td:nth-child(1) { width: 80%; }
+    td:nth-child(2) { width: 10%; }
+    td:nth-child(3) { width: 10%; }
 `;
 
 export default props => {
@@ -75,6 +217,7 @@ export default props => {
 
     return(
         <div>
+            <FontAwesomeIcon icon={faChevronDown}/>
             <ProtocolPhotos protocol={props.protocol}/>
             <ProtocolInfoSection>
                 <SectionHeader>
@@ -85,7 +228,8 @@ export default props => {
                 </SectionHeader>
                 <ProtocolDetails>
                     <h1>СЕКЦИОННАТА ИЗБИРАТЕЛНА КОМИСИЯ УСТАНОВИ</h1>
-                    <table>
+                    <ProtocolDetailsTable>
+                    <tbody>
                         <tr>
                             <td>А. Брой на бюлетините, получени по реда на чл. 215, ал. 1, т. 2 от ИК</td>
                             <td>
@@ -95,12 +239,14 @@ export default props => {
                                 />
                             </td>
                         </tr>
-                    </table>
-                    <button>Приеми</button>
-                    <button>Отхвърли</button>
+                    </tbody>
+                    </ProtocolDetailsTable>
+                    <AcceptButton>Приеми</AcceptButton>
+                    <RejectButton>Отхвърли</RejectButton>
                     <hr/>
                     <h1>ДАННИ ОТ ИЗБИРАТЕЛНИ СПИСЪЦИ</h1>
-                    <table>
+                    <ProtocolDetailsTable>
+                    <tbody>
                         <tr>
                             <td>1. Брой на избирателите според част І и част ІІ на избирателния списък при предаването му на СИК</td>
                             <td>
@@ -128,13 +274,15 @@ export default props => {
                                 />
                             </td>
                         </tr>
-                    </table>
-                    <button>Приеми</button>
-                    <button>Отхвърли</button>
+                    </tbody>
+                    </ProtocolDetailsTable>
+                    <AcceptButton>Приеми</AcceptButton>
+                    <RejectButton>Отхвърли</RejectButton>
                     <hr/>
                     <h1>ДАННИ ИЗВЪН ИЗБИРАТЕЛНИ СПИСЪЦИ И СЪДЪРЖАНИЕТО НА ИЗБИРАТЕЛНАТА КУТИЯ</h1>
                     <h2>4. Бюлетини извън избирателната кутия</h2>
-                    <table>
+                    <ProtocolDetailsTable>
+                        <tbody>
                         <tr>
                             <td>4.а) брой на неизползваните бюлетини</td>
                             <td>
@@ -189,12 +337,14 @@ export default props => {
                                 />
                             </td>
                         </tr>
-                    </table>
-                    <button>Приеми</button>
-                    <button>Отхвърли</button>
+                        </tbody>
+                    </ProtocolDetailsTable>
+                    <AcceptButton>Приеми</AcceptButton>
+                    <RejectButton>Отхвърли</RejectButton>
                     <hr/>
                     <h1>СЛЕД КАТО ОТВОРИ ИЗБИРАТЕЛНАТА КУТИЯ, СИК УСТАНОВИ</h1>
-                    <table>
+                    <ProtocolDetailsTable>
+                        <tbody>
                         <tr>
                             <td>5. Брой на намерените в избирателната кутия бюлетини</td>
                             <td>
@@ -240,12 +390,14 @@ export default props => {
                                 />
                             </td>
                         </tr>
-                    </table>
-                    <button>Приеми</button>
-                    <button>Отхвърли</button>
+                        </tbody>
+                    </ProtocolDetailsTable>
+                    <AcceptButton>Приеми</AcceptButton>
+                    <RejectButton>Отхвърли</RejectButton>
                     <hr/>
                     <h1>8. РАЗПРЕДЕЛЕНИЕ НА ГЛАСОВЕТЕ ПО КАНДИДАТСКИ ЛИСТИ</h1>
-                    <table>
+                    <PartyResultsTable>
+                        <tbody>
                         <tr>
                             <td>{partyDropDown(0)}</td>
                             <td><input type="text" value="3"/></td>
@@ -338,19 +490,20 @@ export default props => {
                         </tr>
                         <tr>
                             <td colSpan="3">
-                                <button>Добави</button>
+                                <button><FontAwesomeIcon icon={faPlus}/> Добави</button>
                             </td>
                         </tr>
                         <tr>
-                            <td colSpan="2">9. Бюлетини, в които не е отбелязан вотът на избирателя (празни бюлетини), бюлетини, в които е гласувано в повече от едно квадратче; бюлетини, в които не може да се установи еднозначно вотът на избирателя и други видове недействителни гласове</td>
-                            <td><input type="text" value="0"/></td>
+                            <td>9. Бюлетини, в които не е отбелязан вотът на избирателя (празни бюлетини), бюлетини, в които е гласувано в повече от едно квадратче; бюлетини, в които не може да се установи еднозначно вотът на избирателя и други видове недействителни гласове</td>
+                            <td colSpan="2"><input type="text" value="0"/></td>
                         </tr>
-                    </table>
-                    <button>Приеми</button>
-                    <button>Отхвърли</button>
+                        </tbody>
+                    </PartyResultsTable>
+                    <AcceptButton>Приеми</AcceptButton>
+                    <RejectButton>Отхвърли</RejectButton>
                     <hr/>
-                    <button>Приеми протокол</button>
-                    <button>Отхвърли протокол</button>
+                    <AcceptButton style={{width: '300px'}}>Приеми протокол</AcceptButton>
+                    <RejectButton style={{width: '300px'}}>Отхвърли протокол</RejectButton>
                 </ProtocolDetails>
             </ProtocolInfoSection>
         </div>
