491f98f16e2780b85d481323503d2241f14bd94d
Blagoslav Mihaylov <az@blago.io>
2021-02-03 17:20:30 +0200

    Fix webpack config and package-lock.json

--- webpack/build.config.js
+++ webpack/build.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const baseConfig = require('./base.config.js');
 
 module.exports = merge(baseConfig, {
