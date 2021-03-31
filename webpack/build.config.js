74608866e27ecb1f242e7c3b743cfe87e87aa953
Haralan Dobrev <harry@clippings.com>
2021-04-01 00:22:50 +0300

    Log env to debug GH Actions

--- webpack/build.config.js
+++ webpack/build.config.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 const baseConfig = require('./base.config.js');
 
 module.exports = env => {
+    console.log(env);
     return merge(baseConfig, {
         mode: 'production',
         plugins: [
