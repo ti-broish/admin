dbc0e3c288ae4de3c47187391a1995df11fd234e
Haralan Dobrev <harry@hkdobrev.com>
2023-02-27 00:57:46 +0200

    Freshen up deps, build and deploy configuration

--- webpack/build.config.js
+++ webpack/build.config.js
@@ -7,16 +7,6 @@ module.exports = env => {
     return merge(baseConfig, {
         mode: 'production',
         plugins: [
-            new webpack.DefinePlugin({
-                'process.env': {
-                    'NODE_ENV': JSON.stringify('production'),
-                    'API_HOST': JSON.stringify(env['API_HOST']),
-                    'FIREBASE_API_KEY': JSON.stringify(env['FIREBASE_API_KEY']),
-                    'FIREBASE_AUTH_DOMAIN': JSON.stringify(env['FIREBASE_AUTH_DOMAIN']),
-                    'FIREBASE_DATABASE_URL': JSON.stringify(env['FIREBASE_DATABASE_URL']),
-                    'FIREBASE_PROJECT_ID': JSON.stringify(env['FIREBASE_PROJECT_ID']),
-                }
-            }),
             new webpack.LoaderOptionsPlugin({minimize: true}),
         ]
     });
--- webpack/serve.config.js
+++ webpack/serve.config.js
@@ -12,17 +12,5 @@ module.exports = env => {
             historyApiFallback: true,
             port: 3000,
         },
-        plugins: [
-            new webpack.DefinePlugin({
-                'process.env': {
-                    'NODE_ENV': JSON.stringify('development'),
-                    'API_HOST': JSON.stringify(env['API_HOST']),
-                    'FIREBASE_API_KEY': JSON.stringify(env['FIREBASE_API_KEY']),
-                    'FIREBASE_AUTH_DOMAIN': JSON.stringify(env['FIREBASE_AUTH_DOMAIN']),
-                    'FIREBASE_DATABASE_URL': JSON.stringify(env['FIREBASE_DATABASE_URL']),
-                    'FIREBASE_PROJECT_ID': JSON.stringify(env['FIREBASE_PROJECT_ID']),
-                }
-            }),
-        ]
     });
 };
